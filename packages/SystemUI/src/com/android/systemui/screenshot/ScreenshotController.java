359848e1d03db4dfdbe41db940cf44dc5ab646ba
Ido Ben-Hur <idoybh2@gmail.com>
2023-02-10 15:59:38 +0200

    SystemUI: screenshot: Also post a notification with a preview
    Make each notification unique so user can take many and still quick access em all
    This does not need a setting side as the notification channel is blockable
    No notification actions for now -
    the file can be accessed by pressing the notification. Gallery can manage it just fine
    
    Squashed:
    
        From: Ido Ben-Hur <idoybh2@gmail.com>
        Date: Tue, 27 Jun 2023 13:35:27 +0300
        Subject: SystemUI: screenshot: Add delete and share notification actions
    
        Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>
    
        From: Ido Ben-Hur <idoybh2@gmail.com>
        Date: Tue, 19 Dec 2023 10:49:58 +0200
        Subject: SystemUI: screenshot: Group the post action notifications
    
        And close the QS shade when the last is deleted
    
        Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>
    
        From: Ido Ben-Hur <idoybh2@gmail.com>
        Date: Mon, 15 Jan 2024 19:33:41 +0200
        Subject: base: SystemUI: Also use TAG to count group notifications
    
        So screenshot / screenrecord don't count each other's notifications.
        Fixes non dismissing group summary when there is a notification from the other service
        Also improve some code while at it
    
        Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>
    
    Fixes: https://github.com/crdroidandroid/issue_tracker/issues/370
    Change-Id: I24af5868f6cf4feb287257c8ef93f47e09e555a9
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- packages/SystemUI/src/com/android/systemui/screenshot/ScreenshotController.java
+++ packages/SystemUI/src/com/android/systemui/screenshot/ScreenshotController.java
@@ -180,7 +180,7 @@ public class ScreenshotController {
     /**
      * Structure returned by the SaveImageInBackgroundTask
      */
-    static class SavedImageData {
+    public static class SavedImageData {
         public Uri uri;
         public Notification.Action deleteAction;
         public List<Notification.Action> smartActions;
@@ -1154,6 +1154,8 @@ public class ScreenshotController {
                 } else {
                     doPostAnimation(imageData);
                 }
+                mNotificationsController.showPostActionNotification(
+                    imageData, mScreenBitmap);
             });
         }
     }
